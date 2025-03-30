d8249917b970ae540ff890bbeac63e5195c62f34
huojian-jan <yasenmaimaiti199634@gmail.com>
2025-03-30 17:06:25 +0800

    创建后端项目

---
+++ uygurtools/backend/aitools/src/test/java/com/example/aitools/AitoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aitools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AitoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
